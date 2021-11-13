720040bfe56be8bd118c92f139405a39f94ccdf6
Yashi002 <50695892+Yashi002@users.noreply.github.com>
2021-11-13 17:32:38 +0530

    Add files via upload

---
+++ db/models.js
@@ -0,0 +1,34 @@
+
+
+const Sequelize = require('sequelize');
+const dbConfig = require('./dbConfig');
+const queries = require('./queries');
+
+const db = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.DIALECT,
+  port: dbConfig.PORT
+});
+
+const User = db.define('user', {
+  username: {
+    type: Sequelize.DataTypes.STRING(25),
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+  }
+})
+
+db.sync({force: process.env.FORCE}).then(() => {
+  console.log("Database Configured");
+})
+
+module.exports = {
+  db,
+  models: {
+    User
+  }
+};
---
+++ utils.js
@@ -0,0 +1,14 @@
+
+
+const errorFunction = (req, res) => {
+  return (err) => {
+    console.log(err);
+    res.status(500).send({
+      success: false,
+      error: err
+    });
+  }
+}
+module.exports = {
+  errorFunction
+}
